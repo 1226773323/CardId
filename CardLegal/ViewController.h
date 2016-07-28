2ba197da2c323b8f0365f362fdd58db8ca78b479
1226773323 <1226773323@qq.com>
2016-07-28 20:45:20 +0800

    CardId
    CardId

---
+++ CardLegal/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  CardLegal
+//
+//  Created by LiLong on 16/7/28.
+//  Copyright © 2016年 ecshcec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *cardIdTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultsLable;
+
+@end
+
---
+++ CardLegal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CardLegal
+//
+//  Created by LiLong on 16/7/28.
+//  Copyright © 2016年 ecshcec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
